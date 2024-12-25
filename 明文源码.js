b1afebd0ce421418136028394ed95b2b2977183e
wenyy168 <leelam2012@163.com>
2024-12-26 01:01:54 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'ea0cf5c6-6bb7-43ac-ab07-a52c7625c293';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
